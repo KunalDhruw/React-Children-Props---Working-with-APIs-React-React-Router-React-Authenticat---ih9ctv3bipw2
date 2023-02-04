31b8fa63b852fc13f141079969325201384a0c58
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-02-04 11:21:20 +0530

    Update ChildComponent.js

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-const ChildComponent = () => {
-//code here
+const ChildComponent = (props) => {
+  //code here
+  const [val, setval] = useState("");
+
+  useEffect(() => {
+    setval(props.input * props.input);
+  }, [props.input]);
+
   return (
     <div id="child">
-      Find The Square <br/>
-     
+      Find The Square <br />
+      {/* <p id="output">{val}</p> */}
     </div>
-  )
-}
-
+  );
+};
 
 export default ChildComponent;
